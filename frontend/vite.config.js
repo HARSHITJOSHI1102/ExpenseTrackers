145b2cedc2c3ac1db6250ca3a4a87cf386920d9d
Harshit <harshitjoshi1102@gmail.com>
2025-05-18 02:08:09 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  build: {
-    outDir: path.resolve(__dirname, '../backend/frontend/dist'), // so backend can serve it
-    emptyOutDir: true
-  }
 });
