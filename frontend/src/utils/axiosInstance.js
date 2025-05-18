e43519c32e62f00bf22c722a5a64deb58359bf5d
HARSHITJOSHI1102 <harshitjoshi1102@gmail.com>
2025-05-18 15:39:31 +0530

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create Axios instance with base config
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://expensetrackers-rot9.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
